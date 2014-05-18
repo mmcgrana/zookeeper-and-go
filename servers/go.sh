1fb21299c33c72a9c936ff7034561524cef619c1
Mark McGranaghan <mmcgrana@gmail.com>
2014-05-18 10:53:20 -0700

    Pass at full setup

---
+++ servers/go.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+apt-get install python-software-properties
+add-apt-repository ppa:duh/golang
+apt-get update
+apt-get install -y golang git-core
+
+cat > /home/vagrant/.profile <<EOF
+export GOPATH=\$HOME
+export PATH=\$PATH:/home/vagrant/bin
+export ZOOKEEPER_SERVERS=192.168.12.11:2181,192.168.12.12:2181,192.168.12.13:2181
+EOF
+chown vagrant:vagrant /home/vagrant/.profile
