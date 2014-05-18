deb354c972b68e9b7270c63e3e806f2ef5cbd6d4
Mark McGranaghan <mmcgrana@gmail.com>
2014-05-18 07:45:03 -0700

    wip

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
-sudo apt-get install -y openjdk-7-jdk
-sudo apt-get install -y zookeeper
-/usr/share/zookeeper/bin/zkServer.sh start
+apt-get update
+apt-get install -y openjdk-7-jdk
+wget -q -O /opt/zookeeper-3.4.6.tar.gz http://apache.mirrors.pair.com/zookeeper/zookeeper-3.4.6/zookeeper-3.4.6.tar.gz
+tar -xzf /opt/zookeeper-3.4.6.tar.gz -C /opt
+cp /opt/zookeeper-3.4.6/conf/zoo_sample.cfg /opt/zookeeper-3.4.6/conf/zoo.cfg
+/opt/zookeeper-3.4.6/bin/zkServer.sh start
