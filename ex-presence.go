876c50c9f5bfe7949639054b8c1f06a3947efb4d
Mark McGranaghan <mmcgrana@gmail.com>
2014-05-25 10:26:29 -0700

    Add presence example for experiments

---
+++ ex-presence.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+	"github.com/samuel/go-zookeeper/zk"
+)
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func connect() *zk.Conn {
+	zksStr := os.Getenv("ZOOKEEPER_SERVERS")
+	zks := strings.Split(zksStr, ",")
+	conn, _, err := zk.Connect(zks, time.Second)
+	must(err)
+	return conn
+}
+
+func main() {
+	conn := connect()
+	defer conn.Close()
+
+	fmt.Println("presence.create")
+	acl := zk.WorldACL(zk.PermAll)
+	_, err := conn.Create("/presence", []byte("here"), zk.FlagEphemeral, acl)
+	must(err)
+
+	<-make(chan bool)
+}
