56c2c1fcdcc9fb4421285a3db05cefa512347b68
Mark McGranaghan <mmcgrana@gmail.com>
2014-05-26 10:26:07 -0700

    Install lib/cli programatically

--- vm-go.sh
+++ vm-go.sh
@@ -13,3 +13,6 @@ export PATH=\$HOME/bin:/opt/go/bin:\$PATH
 export ZOOKEEPER_SERVERS=192.168.12.11:2181,192.168.12.12:2181,192.168.12.13:2181
 EOF
 chown vagrant:vagrant /home/vagrant/.profile
+
+sudo -u vagrant -i go get github.com/samuel/go-zookeeper/zk
+sudo -u vagrant -i go get github.com/mmcgrana/zk
